6713ea04811a431cd0a226ebf74dece1a923ceb8
Andrew West <57451860+andrewhwest@users.noreply.github.com>
2019-11-14 19:30:12 +0000

    Update Permutation Missing Element 2.py

--- Lesson 3 - Time Complexity/Permutation Missing Element 2.py
+++ Lesson 3 - Time Complexity/Permutation Missing Element 2.py
@@ -1,7 +1,8 @@
 def missing_value2(A):
     """Returns missing integer n from A, where n lies in [1, len(A)+1]"""
     
-    sorted_A = A.sort()
-    missing = min(y for x,y in zip(A[1:], A) if x - y == 2) 
+    A.sort()
+    #look for a difference of 2 between adjacent values in sorted A
+    value_before_missing = min(y for x,y in zip(A[1:], A) if x - y == 2) 
     
-    return missing + 1
+    return value_before_missing + 1
