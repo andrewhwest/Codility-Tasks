594a5e70ca8f25433518907e09476658c18a0495
Andrew West <57451860+andrewhwest@users.noreply.github.com>
2019-11-14 22:20:16 +0000

    Update Tape Equilibrium (Naive).py

--- Lesson 3 - Time Complexity/Tape Equilibrium (Naive).py
+++ Lesson 3 - Time Complexity/Tape Equilibrium (Naive).py
@@ -1,4 +1,6 @@
 def minimum_difference(A):
-    """Returns the minimum tape difference"""
+      """Returns the minimum of 
+       |(A[0] + A[1] + ... + A[P-1]) - (A[P] + A[P+1] + ... + A[len(A)-1]) | 
+       for P in [1, len(A)-1] """
     
     return min(abs(sum(A[:P]) - sum(A[P:])) for P in range(1, len(A)))
