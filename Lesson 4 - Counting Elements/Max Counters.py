90e17532dd6077ee6e160f4b2699b318dd8af1cb
Andrew West <ahwest@yahoo.com>
2019-11-24 10:34:35 +0000

    Exception case added

--- Lesson 4 - Counting Elements/Max Counters.py
+++ Lesson 4 - Counting Elements/Max Counters.py
@@ -1,16 +1,30 @@
+class Counter_error(Exception):
+    """Raised when a value in A isn't in [1,N+1]"""
+    pass
+
 def final_counter_values(N, A):
     """Returns the final value of the N counters
        after using counter operations determined by A"""
     
+    #initialise counters
     counters = [0] * N
     
     for value in A:
-        #try:
+        try:
+            
+            #increase(X) operation
             if 1 <= value <= N:
                 counters[value-1] += 1
+                
+            #max counter operaton
             elif value == (N+1):
                 counters = [max(counters)] * N
-        #except value not in range(1,N+1):
-           # print("{0} is an invalid input for our counter operations".format(value))
+                
+            #value not in [1,N+1]
+            else:
+                raise Counter_error
+                
+        except Counter_error:
+            print("{0} is an invalid input for our counter operations".format(value))
             
     return counters
