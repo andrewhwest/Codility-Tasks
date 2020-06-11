8889c673293f35d4503c6fb61631095e17b48d03
Andrew West <ahwest@yahoo.com>
2020-06-11 21:01:36 +0100

    Tidy up, added timer for odd occurrenes task

--- Lesson 2 - Arrays/Odd Occurrences in a List.py
+++ Lesson 2 - Arrays/Odd Occurrences in a List.py
@@ -1,18 +1,18 @@
-def count_occurrences(A):
-    """Creates a dictionary of the form {value in A : number of occurrences}, for a list of integers A"""
+def count_occurrences(integer_list):
+    """Creates a dictionary of the form {value in A : number of occurrences}, for a list of integers"""
 
     value_occurrences = {}
-    for value in A:
+    for value in integer_list:
         value_occurrences[value] = value_occurrences.get(value, 0) + 1
-    
-    return value_occurrences 
-    
-def unpaired_value(A):
-    """Returns the only value that appears once in the list A"""
-           
-    for value, occurrence in count_occurrences(A).items():
+
+    return value_occurrences
+ 
+def unpaired_value(integer_list):
+    """Returns the only value that appears once in a list of integers"""
+
+    for value, occurrence in count_occurrences(integer_list).items():
         if occurrence == 1:
             return value
-    
+    return False   
 
 
